25bae94a44c3d9150f877764988423804ac50e67
Mon4ik <58977309+Mon4ik@users.noreply.github.com>
2020-08-14 20:05:49 +0300

    1.0.0

---
+++ api.lua
@@ -0,0 +1,59 @@
+local api = {}
+-- {path = "rom", icon = nil}
+function api.Create(linkName, filePath, iconPath) --return table{bool Error, string InvalidArgument}
+	if     type(linkName) ~= "string" then
+		local err = {true, "linkName"}
+	elseif type(filePath) ~= "string" then
+		local err = {true, "filePath"}
+	else
+		local err = {false}
+		if iconPath == nil then
+			iconPath = "nil"
+		end
+		local lnk = fs.open(linkName, "w")
+		lnk.write("{path = \"".. filePath .."\", icon = "..iconPath.."}")
+		lnk.close()	
+		local err = {false}
+	end
+	return err
+end
+function api.openLnk(lnkFile)
+	if type(lnkFile) ~= "string" then
+		local err = {true, "linkName"}
+	else
+		local err = {false}
+		local file = fs.open(lnkFile, "r")
+		local data = file.readAll()
+		file.close()
+		local dataa = assert(loadstring("return "..data))()
+		shell.setDir(dataa.path)
+	end
+	return err
+end
+function api.getIconPath(lnkFile)
+	if type(lnkFile) ~= "string" then
+		local err = {true, "linkName"}
+	else
+		local err = {false}
+		local file = fs.open(lnkFile, "r")
+		local data = file.readAll()
+		file.close()
+		local data = assert(loadstring("return "..data))()
+		return data.icon
+	end
+	return err
+end
+function api.getPath(lnkFile)
+	if type(lnkFile) ~= "string" then
+		local err = {true, "linkName"}
+	else
+		local err = {false}
+		local file = fs.open(lnkFile, "r")
+		local data = file.readAll()
+		file.close()
+		local data = assert(loadstring("return "..data))()
+		return data.path
+	end
+	return err
+end
+return api
